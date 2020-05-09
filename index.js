741c25910c902e9f52d16eddd817f3339eb392ff
labaertasche <labaertasche@gmail.com>
2020-05-09 23:28:20 +0200

    3 files modified

--- index.js
+++ index.js
@@ -2,3 +2,31 @@
 import './style.css';
 
 // Eigenes JavaScript unten drunter :)
+//Wenn Knopfdruck: 
+// - aus den 2 Feldern Informationen lesen
+// - in HTML Element verpacken
+// - wieder als Kommentar ausgebeben
+
+var kommentarbereich = document.getElementById('kommentarbereich') 
+
+var submitKnopf = document.getElementById ('kommentar-form-submit')
+submitKnopf.addEventListener('click',function(event){
+var name = document.getElementById('kommentar-form-name')
+var meinung = document.getElementById('kommentar-form-meinung')
+console.log(name.value, meinung.value)
+
+var kommentar = ''
+
+kommentar += '<section class ="kommentar">'
+
+kommentar += '<span>' + name.value + '</span>'
+
+kommentar += '<p>' + meinung.value + '</p>'
+
+kommentar += '</section>' 
+
+console.log(kommentar)
+console.log(kommentarbereich)
+
+kommentarbereich.innerHTML += kommentar
+})
